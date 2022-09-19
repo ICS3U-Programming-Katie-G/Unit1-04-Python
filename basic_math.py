b1fda96b23d7b57626864820c792b496f1ee341d
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-19 15:01:03 +0000

    coded a program to execute basic math functions in Python

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Katie
+# Date: September 19th, 2022
+# This program does basic math equations.
+
+def main():
+    #this function does basic math equations.
+    print("18+3={}".format(18+3))
+    print("8-4={}".format(8-4))
+    print("6/3={}".format(6/3))
+    print("6*7={}".format(6*7))
+    print("5**2+10={}".format(5**2+10))
+    print("5*6+7={}".format(5*6+7))
+    print("30-10/2={}".format(30-10/2))
+
+
+if __name__ == "__main__":
+    main()
