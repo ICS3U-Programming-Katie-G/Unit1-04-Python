cb5973338c1253d5d3aae829c1fe3264b73a8f74
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-21 19:05:30 +0000

    finished coding this while listening to a playthrough of Detroit Become Human. 10/10 would recommend. Excellent vibes.

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created By: Katie
+# Date: September 19th, 2022
+# This program calculates the area and perimeter of a rectangle.
+
+
+def main():
+    # this function calculates the area and perimeter of a rectangle with the pre-set dimensions.
+    print("If a rectangle has the dimensions:")
+    print("7cm x 4cm")
+    print("")
+    print("The area is {}cm^2".format(7 * 4))
+    print("The perimeter is {}cm".format(2 * (7 + 4)))
+
+
+if __name__ == "__main__":
+    main()
--- basic_math.py
+++ basic_math.py
@@ -4,15 +4,16 @@
 # Date: September 19th, 2022
 # This program does basic math equations.
 
+
 def main():
-    #this function does basic math equations.
-    print("18+3={}".format(18+3))
-    print("8-4={}".format(8-4))
-    print("6/3={}".format(6/3))
-    print("6*7={}".format(6*7))
-    print("5**2+10={}".format(5**2+10))
-    print("5*6+7={}".format(5*6+7))
-    print("30-10/2={}".format(30-10/2))
+    # this function does basic math equations.
+    print("18+3={}".format(18 + 3))
+    print("8-4={}".format(8 - 4))
+    print("6/3={}".format(6 / 3))
+    print("6*7={}".format(6 * 7))
+    print("5**2+10={}".format(5**2 + 10))
+    print("5*6+7={}".format(5 * 6 + 7))
+    print("30-10/2={}".format(30 - 10 / 2))
 
 
 if __name__ == "__main__":
